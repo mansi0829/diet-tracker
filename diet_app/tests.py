68457df02f762b644356fe2040dc26fbb4fd1d8f
mansi0829 <mansigundre1@gmail.com>
2023-06-27 11:03:26 +0530

    complete

---
+++ diet_app/tests.py
@@ -0,0 +1,32 @@
+from django.test import TestCase, Client
+from .models import User, FoodCategory
+
+
+class UserModelUnitTestCase(TestCase):
+    def setUp(self):
+        self.user = User.objects.create(email='john@test.com', username='John', password='pass123')
+
+    def test_user_model(self):
+        data = self.user
+        self.assertTrue(isinstance(data, User))
+        self.assertIsInstance(data, User)
+        self.assertEqual(str(data.username), 'John')
+
+
+class FoodCategoryUnitTestCase(TestCase):
+    def setUp(self):
+        self.category = FoodCategory.objects.create(category_name='Vegetables')
+
+    def test_food_category_model(self):
+        data = self.category
+        self.assertIsInstance(data, FoodCategory)
+        self.assertEqual(str(data.category_name), 'Vegetables')
+
+
+class IndexRequestTestCase(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_index_view(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
---
+++ readme.md
@@ -0,0 +1,24 @@
+# Diet tracker
+
+How it will work,
+
+- `git clone git@github.com:Credenz-Web-Weaver/diet-tracker.git` --- To clone the project on your pc
+
+- `pip install -r "requirements.txt"` --- To install the required packages for the project
+
+- `python -m django runserver` --- To start server
+
+- Then Open `http://127.0.0.1:8000/`
+
+### Currently this application do:
+
+- [x] The basic django project is created for you follow above steps to start the project
+- [x] The project will be running locally on http://127.0.0.1:8000/
+
+### Your task is to -
+
+Updation to be done:
+
+- [ ] Create a form that will take the necessary input for food item to keep it day wise
+- [ ] Display a table view to show the nutrients breakdown for the food item
+- [ ] Show some graphical representation of this data
